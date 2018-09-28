7bbbea2ae0ce9982ff30fad0937d2128227b5abe
Ahchuang <E0036072@u.nus.edu>
2018-09-28 13:02:52 +0800

    update tables

--- tables.sql
+++ tables.sql
@@ -1,55 +1,48 @@
 DROP DATABASE IF EXISTS ourdatabase;
 CREATE DATABASE ourdatabase;
 
-DROP TABLE IF EXISTS drivers;
-CREATE TABLE drivers (
-  ID VARCHAR(128) PRIMARY KEY,
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+  username VARCHAR(128) PRIMARY KEY,
   password VARCHAR(128) NOT NULL,
   name VARCHAR(64) NOT NULL,
-  age INTEGER NOT NULL check(age >= 21 AND age <= 70),
-  sex VARCHAR(8) NOT NULL check(sex = 'male' OR sex = 'female')
+  age INTEGER NOT NULL,
+  sex VARCHAR(8) NOT NULL check(sex = 'male' OR sex = 'female'),
+  role VARCHAR(10) NOT NULL check(role = 'admin' OR role = 'driver' OR role = 'passenger')
 );
 
-DROP TABLE IF EXISTS cars;
+DROP TABLE IF EXISTS cars CASCADE;
 CREATE TABLE cars (
-  ID CHAR(8) PRIMARY KEY,
+  plate_number CHAR(8) PRIMARY KEY,
   driver VARCHAR(128),
   capacity INT NOT NULL,
   model VARCHAR(64),
-  FOREIGN KEY (driver) REFERENCES drivers(ID) ON DELETE CASCADE
+  FOREIGN KEY (driver) REFERENCES users(username) ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS rides;
+DROP TABLE IF EXISTS rides CASCADE;
 CREATE TABLE rides (
-  ID VARCHAR(64) PRIMARY KEY,
   car CHAR(8),
-  start_time TIMESTAMP NOT NULL,
-  source VARCHAR(128) NOT NULL,
-  destination VARCHAR(128) NOT NULL,
-  capacity INT NOT NULL,
+  start_time TIMESTAMP,
+  source VARCHAR(128),
+  destination VARCHAR(128),
   number_passenger INT,
   status VARCHAR(8) NOT NULL CHECK(status = 'pending' OR status = 'success' OR status = 'failed'),
+  PRIMARY KEY (car, start_time, source, destination),
   CHECK (source <> destination),
-  CHECK (number_passenger >= 0 AND number_passenger <= capacity),
-  FOREIGN KEY (car, capacity) REFERENCES cars(ID, capacity)
+  FOREIGN KEY (car) REFERENCES cars(plate_number)
 );
 
 DROP TABLE IF EXISTS bids;
 CREATE TABLE bids (
   passenger VARCHAR(128),
-  ride VARCHAR(64),
+  car CHAR(8),
+  start_time TIMESTAMP,
+  source VARCHAR(128),
+  destination VARCHAR(128),
   amount FLOAT NOT NULL CHECK (amount >= 0),
   status VARCHAR(8) NOT NULL CHECK(status = 'pending' OR status = 'success' OR status = 'failed'),
-  PRIMARY KEY (passenger, ride),
-  FOREIGN KEY (passenger) REFERENCES passengers(ID) ON DELETE CASCADE,
-  FOREIGN KEY (ride) REFERENCES rides(ID) ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS passengers;
-CREATE TABLE passengers(
-  ID VARCHAR(128) PRIMARY KEY,
-  password VARCHAR(128) NOT NULL,
-  name VARCHAR(64) NOT NULL,
-  age INTEGER NOT NULL,
-  sex VARCHAR(8) NOT NULL check(sex = 'male' OR sex = 'female')
+  PRIMARY KEY (passenger, car, start_time, source, destination),
+  FOREIGN KEY (passenger) REFERENCES users(username) ON DELETE CASCADE,
+  FOREIGN KEY (car, start_time, source, destination) REFERENCES rides(car, start_time, source, destination) ON DELETE CASCADE
 );
